93e321f77b72bbdc238a007541591cbeaaec555e
Harshit Mishra <165307122+1harshit234@users.noreply.github.com>
2024-05-26 06:19:46 -0700

    add
    in this we have added two sets in the python

---
+++ add.py
@@ -0,0 +1,8 @@
+sample_set = {"Yellow", "Orange", "Black"}
+sample_list = ["Blue", "Green", "Red"]
+
+y = sample_set.union(sample_list)
+sample_set.update(sample_list)
+print(y)
+print(sample_set)
+#we can add the two set using union
